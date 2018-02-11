583ecb33fc1720a589b232bb51588eb107073431
Veini Tuominen <veini.tuominen@hotmail.com>
2018-02-11 16:08:50 +0200

    Tehtävä 10

--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -1,6 +1,15 @@
 const supertest = require('supertest')
 const { app, server } = require('../index')
+const Blog = require('../models/blog')
+
 const api = supertest(app)
+
+const cleanBlog = ({ title, author, url, likes }) => ({
+	title,
+	author,
+	url,
+	likes
+})
 
 describe('api tests', () => {
 	test('blogs are returned as json', async () => {
@@ -16,6 +25,29 @@ describe('api tests', () => {
 			.expect(201)
 			.expect('Content-Type', /application\/json/)
 	})
+
+	test('created blog has 0 likes if not specified', async () => {
+		const blog = {
+			title: 'C++',
+			author: 'Bjarne Stroustrup',
+			url: 'library'
+		}
+
+		await api
+			.post('/api/blogs')
+			.send(blog)
+			.expect(201)
+			.expect('Content-Type', /application\/json/)
+
+		const response = await api.get('/api/blogs')
+
+		expect(response.body.map(blog => cleanBlog(blog)))
+			.toContainEqual({ ...blog, likes: 0 })
+	})
+})
+
+beforeAll(async () => {
+	await Blog.remove({})
 })
 
 afterAll(() => {

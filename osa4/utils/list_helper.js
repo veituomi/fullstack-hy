31bf2a543677c6a1dff9202462eda4123890ef4f
Veini Tuominen <veini.tuominen@hotmail.com>
2018-02-11 13:43:19 +0200

    Tehtävä 4.4

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
 	return 1
 }
 
+const totalLikes = (blogs) => {
+	return blogs
+		.map(blog => blog.likes)
+		.reduce((a, b) => a + b, 0)
+}
+
 module.exports = {
-	dummy
+	dummy,
+	totalLikes
 }
